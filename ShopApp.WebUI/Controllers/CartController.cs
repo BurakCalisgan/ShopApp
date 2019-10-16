b47188b3a08aa482274e4fe7c7f3547e3caf8a25
Burak Çalışgan <calisganburak@gmail.com>
2019-10-16 22:26:39 +0300

    [TR] Cart View hazırlandı. [EN] Cart View has been prepared.

---
+++ ShopApp.WebUI/Controllers/CartController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApp.WebUI.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+
+        #region Index
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region AddToCart
+
+        [HttpPost]
+        public IActionResult AddToCart()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
