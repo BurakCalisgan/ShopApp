903e360dc85d96e6222c465fa7897a8bb0465d6f
Burak Çalışgan <calisganburak@gmail.com>
2019-10-16 22:47:56 +0300

    [TR] GetCartItems işlemi gerçekleştirildi. [EN] The GetCartItems transaction has been performed.

--- ShopApp.DataAccess/Concrete/EfCore/EfCartDal.cs
+++ ShopApp.DataAccess/Concrete/EfCore/EfCartDal.cs
@@ -1,12 +1,24 @@
-﻿using ShopApp.DataAccess.Abstract;
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApp.DataAccess.Abstract;
 using ShopApp.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ShopApp.DataAccess.Concrete.EfCore
 {
     public class EfCartDal : EfCoreGenericRepository<Cart, ShopContext>, ICartDal
     {
+        public Cart GetCartByUserId(string userId)
+        {
+            using (ShopContext context = new ShopContext())
+            {
+                return context.Carts
+                              .Include(i => i.CartItems)
+                              .ThenInclude(i => i.Product)
+                              .FirstOrDefault(i => i.UserId == userId);
+            } 
+        }
     }
 }
